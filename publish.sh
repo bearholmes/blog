0f6de4fef3cbc6c7082509b41bb76c8afa2add99
a11y-linkage <a11y.linkage@daumcorp.com>
2019-02-18 13:41:51 +0900

    Update docs

--- publish.sh
+++ publish.sh
@@ -11,6 +11,7 @@ git pull origin master --rebase
 
 # gitbook build로 생긴 _book폴더 아래 모든 정보를 현재 위치로 가져온다.
 cp -R _book/* ./docs
+cp index.html ./docs
 
 # node_modules폴더와 _book폴더를 지워준다.
 git clean -fx node_modules
